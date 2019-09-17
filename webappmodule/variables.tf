f440ef7b9a23eb89a2074d89b3fdd87d9bca2d1e
recetasdevops <44634828+recetasdevops@users.noreply.github.com>
2019-09-17 08:30:30 +0200

    Add files via upload

---
+++ webappmodule/variables.tf
@@ -0,0 +1,28 @@
+variable "appserviceplanname" {
+  description = "Service plan name"
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name"
+  type = string
+}
+
+variable "webappname" {
+  description = "WebAppName"
+  type = string
+}
+
+variable "api_url" {
+  description = "Api url"
+  type = string
+}
+
+variable "versionwebapp" {
+  description = "Version WebApp"
+  type = string
+}
+
+
+
+
